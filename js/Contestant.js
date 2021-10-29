3eea2199e2eba45647eada07d00e6b5a21add8cf
gotbot123 <85640400+gotbot123@users.noreply.github.com>
2021-10-29 18:25:28 +0100

    Add files via upload

---
+++ js/Contestant.js
@@ -0,0 +1,17 @@
+class Contestant {
+  constructor(){
+    this.index = null;
+    this.answer = 0;
+    this.name = null;
+  }
+
+getCount(){
+  var contestantCountRef= database.ref("contestantCount")
+}
+
+updateCount(count){
+database.ref("/").update({
+  contestantCount: count
+})
+}
+}
